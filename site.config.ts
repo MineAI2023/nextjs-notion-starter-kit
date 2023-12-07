ffbffb36e09af1b47615d9701538bc4cf5559934
MineAI2023 <140796330+MineAI2023@users.noreply.github.com>
2023-12-07 22:28:27 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '9ac0c3be3e21460883e1de0400d76bb8',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: '寰宇时空',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Travel Man',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
